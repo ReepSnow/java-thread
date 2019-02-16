bb99ad3730f287407d31f38c8c42216b518be85c
wangpengtao <wangpengtao@ifunq.com>
2019-02-16 20:50:02 +0800

    threadLocal变量源代码查看

--- src/main/java/com/snow/example/App.java
+++ src/main/java/com/snow/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        ThreadLocal<Object> threadLocal1 = new ThreadLocal<>();
+        ThreadLocal<Object> threadLocal2 = new ThreadLocal<>();
+        threadLocal1.set(1);
         System.out.println( "Hello World!" );
     }
 }
